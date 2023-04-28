62c5397c684ef839364a0c12e2b6e63b0a942a4c
jchen6727 <jchen.6727@gmail.com>
2023-04-28 10:54:29 -0400

    update -> colab?

--- CA3/dash_viz.py
+++ CA3/dash_viz.py
@@ -1,6 +1,8 @@
 import pandas
 import utils
 import plotly.express as px
+#import plotly
+#import plotly.graph_objs as go
 from dash import Dash, html, dcc, callback, Output, Input
 
 app = Dash(__name__)
@@ -12,9 +14,9 @@ df['MSE'] = df.apply(utils.mse, axis=1)
 app.layout = html.Div([
     html.H1(children="Fitness (MSE)", style={'textAlign':'center'}),
     dcc.Slider(0.1, 2.5, 0.1,
-               value=1.2,
+               value=1.3,
                id='upper-bound'),
-    dcc.Graph(id='graph', style={'width': '75vh', 'height': '75vh'}, animate=True)
+    dcc.Graph(id='graph', style={'height': '75vh'})
 ])
 
 
@@ -25,7 +27,10 @@ def update_figure(value):
                         x='netParams.connParams.PYR->BC_AMPA.weight',
                         y='netParams.connParams.PYR->OLM_AMPA.weight',
                         z='netParams.connParams.PYR->PYR_AMPA.weight',
-                        color='MSE'
+                        color='MSE',
+                        range_x=[3.6e-5, 0.0036],
+                        range_y=[3.6e-5, 0.0036],
+                        range_z=[2e-6, 0.0002]
                         )
     return fig
 
