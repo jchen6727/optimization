08e60c1e737ce79a6920112d917dea45e8793633
jchen6727 <jchen.6727@gmail.com>
2023-04-28 10:06:08 -0400

    changes to dash viz

--- CA3/dash_viz.py
+++ CA3/dash_viz.py
@@ -5,35 +5,30 @@ from dash import Dash, html, dcc, callback, Output, Input
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H1(children="Fitness (MSE)", style={'textAlign':'center'}),
-    dcc.Slider(0, 50, 5,
-               value=25,
-               id='UB'),
-    html.Div(id='plot')
-])
-
-@app.callback(
-
-
-
-
-)
-def filter_MSE(df: pandas.DataFrame, ub):
-    return df[(self. < ub)]
-
-filenames = ["batch_csv/out{}.csv".format(i) for i in range(11)]
-
-df = pandas.concat(
-    [pandas.read_csv(filename) for filename in filenames]
-)
+df = pandas.read_csv("frequencies.csv")
 df = df.drop(['Unnamed: 0'], axis=1).reset_index(drop=True)
-
 df['MSE'] = df.apply(utils.mse, axis=1)
 
-fig = px.scatter_3d(df,
-                    x='netParams.connParams.PYR->BC_AMPA.weight',
-                    y='netParams.connParams.PYR->OLM_AMPA.weight',
-                    z='netParams.connParams.PYR->PYR_AMPA.weight',
-                    color='MSE'
-                    )
+app.layout = html.Div([
+    html.H1(children="Fitness (MSE)", style={'textAlign':'center'}),
+    dcc.Slider(0.1, 2.5, 0.1,
+               value=1.2,
+               id='upper-bound'),
+    dcc.Graph(id='graph', style={'width': '75vh', 'height': '75vh'}, animate=True)
+])
+
+
+@callback( Output('graph', 'figure'), Input('upper-bound', 'value') )
+def update_figure(value):
+    dff = df[df['MSE'] < value]
+    fig = px.scatter_3d(dff,
+                        x='netParams.connParams.PYR->BC_AMPA.weight',
+                        y='netParams.connParams.PYR->OLM_AMPA.weight',
+                        z='netParams.connParams.PYR->PYR_AMPA.weight',
+                        color='MSE'
+                        )
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- CA3/utils.py
+++ CA3/utils.py
@@ -10,15 +10,13 @@ def mse(run: pandas.Series, values = TARGET.keys()):
     freqs = run[values]
     return numpy.square(TARGET - freqs).mean()
 
-def filter_mse(df: pandas.Dataframe, ub):
-    #return df[df.MSE < ub] #cannot use this line with typing
-	return df[df['MSE'] < ub]
+
+def filter_mse(df: pandas.DataFrame, ub):
+    return df[df['MSE'] < ub] # return df[df.MSE < ub] #cannot use this line with typing
+
 
 def agg_csv(files, target=None):
     df = pandas.concat([pandas.read_csv(file) for file in files]).reset_index(drop=True)  
     if target:
         df.to_csv(target, index=False)
     return df  
- 
-        
-    
