a3d5d61b300098ca6393d468f6ae885768c22822
jchen6727 <jchen6727@users.noreply.github.com>
2023-04-27 22:59:17 -0400

    commit changes
    CA3 optimization viz -- plotly

--- CA3/utils.py
+++ CA3/utils.py
@@ -6,6 +6,19 @@ TARGET = pandas.Series(
      'BC': 14.3,
      'OLM': 4.83})
 
-def mse(run: pandas.Series):
-    freqs = run[['PYR', 'BC', 'OLM']]
+def mse(run: pandas.Series, values = TARGET.keys()):
+    freqs = run[values]
     return numpy.square(TARGET - freqs).mean()
+
+def filter_mse(df: pandas.Dataframe, ub):
+    #return df[df.MSE < ub] #cannot use this line with typing
+		return df[df['MSE'] < ub]
+
+def agg_csv(files, target=None):
+    df = pandas.concat([pandas.read_csv(file) for file in files]).reset_index(drop=True)  
+    if target:
+        df.to_csv(target, index=False)
+    return df  
+ 
+        
+    
