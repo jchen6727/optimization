c8a10f5f1f42a2b60335ef82c436b31266382289
James <jchen.6727@gmail.com>
2023-05-05 09:02:41 -0400

    Auto stash before merge of "main" and "origin/main"

--- HH/init.py
+++
@@ -1,4 +0,0 @@
-from netpyne import sim
-from HHTut import netParams, cfg
-
-sim.createSimulateAnalyze(netParams, cfg)
---
+++ test/test_optuna.py
@@ -0,0 +1,5 @@
+from avatk.runtk.runners import dispatcher, runner
+from avatk.runtk.utils import set_map
+import json
+
+class optuna_runner(runner):
