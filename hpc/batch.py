ebb070598f19af0b7d07410d4d1d51ba68fabbae
James Chen <jchen.6727@gmail.com>
2023-08-31 23:28:48 -0400

    update to work with netpyne coredev
    jobName and jobPath arguments used

--- hpc/batch.py
+++ hpc/batch.py
@@ -3,7 +3,7 @@ from netpyne.batch import Batch
 def slurm(batchCfg):
     template = \
 """#!/bin/bash
-#SBATCH --job-name={simLabel}
+#SBATCH --job-name={jobName}
 #SBATCH -A csd403
 #SBATCH -t 00:30:00
 #SBATCH --nodes=1
@@ -11,15 +11,15 @@ def slurm(batchCfg):
 #SBATCH --mem=32G
 #SBATCH --export=ALL
 #SBATCH --partition=shared
-#SBATCH -o {jobName}.run
-#SBATCH -e {jobName}.err
+#SBATCH -o {jobPath}.run
+#SBATCH -e {jobPath}.err
 #SBATCH --mail-user=jchen.6727@gmail.com
 #SBATCH --mail-type=end
 
 time mpirun -n 4 nrniv -python -mpi batchJob.py simConfig={cfgSavePath} netParams={netParamsSavePath}
 """
-    return {'submit': 'sbatch {jobName}.sh'.format(**batchCfg),
-            'filename': '{jobName}.sh'.format(**batchCfg),
+    return {'submit': 'sbatch {jobPath}.sh'.format(**batchCfg),
+            'filename': '{jobPath}.sh'.format(**batchCfg),
             'script': template.format(**batchCfg)}
 
 
@@ -28,12 +28,13 @@ def runBatch():
     params = {'AMPA': [0.5, 1.0, 1.5]}
     b = Batch(cfgFile='cfg.py', netParamsFile='netParams.py', params=params)
     b.batchLabel = "test"
-    b.saveFolder = 'data/'+b.batchLabel
+    b.saveFolder = "data/test"
     b.method = 'grid'
 
     b.runCfg = {'type': 'custom',
                 'function': slurm,
-                'run': True,
+                'run': False,
+                #'run': True,
     }
     b.run() # run batch
 
